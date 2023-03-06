f08160a194ba85e9c17b886f1ebf7b879a28a8d9
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-06 12:00:42 -0300

    Add recursive search

--- 01_introduction_to_algorithms/binary_search.py
+++ 01_introduction_to_algorithms/binary_search.py
@@ -1,23 +1,56 @@
-def binary_search(list, item):
-    low = 0
-    high = len(list) - 1
+class BinarySearch():
 
-    while(low <= high):
-        mid = (low + high) // 2
-        guess = list[mid]
+    def binary_search(self,list, item):
+        ## Variables to keep track of the list 
+        low = 0
+        high = len(list) - 1
+
+        while(low <= high):
+            ## Checking the mid element
+            mid = (low + high) // 2
+            guess = list[mid]
+            
+            ## Item found
+            if guess == item:
+                return mid
+            
+            ## Too low
+            elif guess < item:
+                low = mid + 1
+            
+            ## Too high
+            else:
+                high = mid - 1
+            
+            ## Item not found
+            return None
         
-        if guess == item:
-            return mid
-        
-        elif guess < item:
-            low = mid + 1
+    def search_recursive(self,list,item, low, high):
+
+        ## Check base case
+        if high >= low:
+            
+            mid = (low + high) // 2
+            guess = list[mid]
+
+            ## Item found 
+            if guess == item:
+                return mid
+            
+            ## Too low
+            elif guess < item:
+                return self.search_recursive(list, item, low, mid - 1)
+            
+            ## Too high
+            else:
+                return self.search_recursive(list, item, mid + 1, high)
         
         else:
-            high = mid - 1
-        
-        return None
+            return None
 
 my_list = [1,3,5,7,9]
 
-print(binary_search(my_list,5))
-print(binary_search(my_list,10))
+bs = BinarySearch()
+
+print(bs.binary_search(my_list,5))
+print(bs.binary_search(my_list,10))
