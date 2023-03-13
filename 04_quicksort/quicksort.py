fb21a7b7c3ce98e2e6920e98d9a79ce8f33018aa
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-12 23:30:42 -0300

    Updade quicksort

--- 04_quicksort/quicksort.py
+++ 04_quicksort/quicksort.py
@@ -1,10 +1,14 @@
 def quicksort(array):
+    # Check if that's the best case, it's already sorted
     if len(array) < 2:
         return array
     else:
+        # Chose the first element as pivot
         pivot = array[0]
+        # Divides it to two arrays, one greater and one less than the pivot
         less = [i for i in array [1: ] if i <= pivot]
         greater = [i for i in array [1: ] if i > pivot]
+        # Concatenate the arrays
         return quicksort(less) + [pivot] + quicksort(greater)
 
 print(quicksort([20,50,1,6,2]))
